56e59235dc430615797f943df431755703af71ae
cmehadji27 <cmehadji27@gmail.com>
2024-03-02 23:26:56 +0100

    mission failed

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -12,8 +12,12 @@ pub fn send_message(mut stream: &TcpStream, message: Message, data: &Vec<u8>) {
     println!("\nserialized:  {}", serialized);
 
     let serialized_size = serialized.len() as u32;
+    println!("serialized_size: {}\n", serialized_size);
+
     let data_size = data.len() as u32;
-    let total_size = serialized_size + data_size;
+    println!("data_size: {}\n", data_size);
+
+    let total_size = serialized_size + data_size ;
     println!("\ntotal_size :  {}", total_size);
 
     // Envoi de la taille totale du message
@@ -21,11 +25,12 @@ pub fn send_message(mut stream: &TcpStream, message: Message, data: &Vec<u8>) {
     println!( "\nStep 1 passed");
 
     // Envoi de la taille du message JSON
-    stream.write_all(&serialized_size.to_be_bytes()).expect("failed to send total size");
+    stream.write_all(&serialized_size.to_be_bytes()).expect("failed to send total size JSON");
     println!( "\nStep 2 passed");
 
+
     // Envoi du message JSON
-    stream.write_all(serialized.as_bytes()).expect("failed to send serialized message");
+    stream.write_all(&serialized.as_bytes()).expect("failed to send serialized message");
     println!( "\nStep 3 passed");
 
     // Envoi des données binaires
@@ -44,44 +49,44 @@ pub fn send_message(mut stream: &TcpStream, message: Message, data: &Vec<u8>) {
 
 pub fn reception_message(mut stream: &TcpStream) {
     println!("Étape 1 : Début de la réception du message");
-let mut size_bytes = [0u8; 4];
-stream.read_exact(&mut size_bytes);
-println!("Étape 2 : Lecture de size_bytes terminée, valeur : {:?}", size_bytes);
+    let mut size_bytes = [0u8; 4];
+    stream.read_exact(&mut size_bytes);
+    println!("Étape 2 : Lecture de size_bytes terminée, valeur : {:?}", size_bytes);
 
-let mut new_size_json = [0; 4];
-stream.read_exact(&mut new_size_json);
-println!("Étape 3 : Lecture de new_size_json terminée, valeur : {:?}", new_size_json);
+    let mut new_size_json = [0; 4];
+    stream.read_exact(&mut new_size_json);
+    println!("Étape 3 : Lecture de new_size_json terminée, valeur : {:?}", new_size_json);
 
-let msg_size = u32::from_be_bytes(new_size_json) as usize;
-println!("Étape 4 : Conversion de new_size_json en usize terminée, valeur : {}", msg_size);
+    let msg_size = u32::from_be_bytes(new_size_json) as usize;
+    println!("Étape 4 : Conversion de new_size_json en usize terminée, valeur : {}", msg_size);
 
-let mut json_buffer = vec![0u8; msg_size as usize];
-stream.read_exact(&mut json_buffer);
-println!("Étape 5 : Lecture du buffer JSON terminée, valeur : {:?}", json_buffer);
+    let mut json_buffer = vec![0u8; msg_size as usize];
+    stream.read_exact(&mut json_buffer);
+    println!("Étape 5 : Lecture du buffer JSON terminée, valeur : {:?}", json_buffer);
 
-let img_data: &mut Vec<u8> = &mut Vec::new();  
-println!("Étape 6 : Initialisation de img_data terminée, valeur : {:?}", img_data);
+    let img_data: &mut Vec<u8> = &mut Vec::new();  
+    println!("Étape 6 : Initialisation de img_data terminée, valeur : {:?}", img_data);
 
-let mut data_binaire_taille = 0;
-if msg_size >= size_bytes.len() {
-    data_binaire_taille = msg_size - size_bytes.len();
-    println!("Étape 7 : Calcul de data_binaire_taille terminé, valeur : {}", data_binaire_taille);
-    println!("msg_size : {}", msg_size);
-    println!("size_bytes.len() : {}", size_bytes.len());
-} else {
-    eprintln!("Erreur Étape 7 : msg_size est plus petit que size_bytes.len()");
-    println!("msg_size : {}", msg_size);
-    println!("size_bytes.len() : {}", size_bytes.len());
-}    
+    let mut data_binaire_taille = 0;
+    if msg_size >= size_bytes.len() {
+        data_binaire_taille = msg_size - size_bytes.len();
+        println!("Étape 7 : Calcul de data_binaire_taille terminé, valeur : {}", data_binaire_taille);
+        println!("msg_size : {}", msg_size);
+        println!("size_bytes.len() : {}", size_bytes.len());
+    } else {
+        eprintln!("Erreur Étape 7 : msg_size est plus petit que size_bytes.len()");
+        println!("msg_size : {}", msg_size);
+        println!("size_bytes.len() : {}", size_bytes.len());
+    }    
 
-if data_binaire_taille > 0 {
-    *img_data = vec![0; data_binaire_taille];
-    stream.read_exact(img_data);
-    println!("Étape 8 : Lecture des données binaires terminée, valeur : {:?}", img_data);
-}
+    if data_binaire_taille > 0 {
+        *img_data = vec![0; data_binaire_taille];
+        stream.read_exact(img_data);
+        println!("Étape 8 : Lecture des données binaires terminée, valeur : {:?}", img_data);
+    }
 
-let message = String::from_utf8_lossy(&json_buffer).into_owned();
-println!("Étape 9 : Conversion du buffer JSON en chaîne de caractères terminée, valeur : {}", message);
+    let message = String::from_utf8_lossy(&json_buffer).into_owned();
+    println!("Étape 9 : Conversion du buffer JSON en chaîne de caractères terminée, valeur : {}", message);
 
     // Traitement du message
     match parse_json_string(&message) {
