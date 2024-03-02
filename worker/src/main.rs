a9426781875a479ecef83dec0cf4767f9402daeb
cmehadji27 <cmehadji27@gmail.com>
2024-03-02 17:19:27 +0100

    marche toujours pas

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -16,31 +16,28 @@ pub fn send_message(mut stream: &TcpStream, message: Message, data: &Vec<u8>) {
     println!("\nserialized_size :  {}", serialized_size);
 
     let size_bytes = serialized_size.to_be_bytes(); 
-    println!("\nsize_bytes : {:?}", size_bytes);
+    println!("\nsize_bytes : {:?} \n", size_bytes);
+
+    // Envoi de la taille totale du message
+    stream.write_all(&size_bytes);
+    println!( "\nStep 1 passed");
     
-      // Envoi de la taille totale du message
-      stream.write_all(&size_bytes);
+    // Envoi de la taille du message JSON
+    stream.write_all(&size_bytes);
+    println!( "\nStep 2 passed");
+    
+    // Envoi du message JSON
+    stream.write_all(serialized.as_bytes());
+    println!( "\nStep 3 passed");
 
-      // Envoi de la taille du message JSON
-      stream.write_all(&size_bytes);
-  
-      // Envoi du message JSON
-      stream.write_all(serialized.as_bytes());
-
-      println!("\ndata.len: {}\n", data.len());
-      if data.len() > 0 {
+    //println!("\ndata: {:?}", data); 
+    
+    if !data.is_empty() {
+        println!("\n yes \n");
         for byte in data {
-          stream.write_all(&[*byte]).expect( "Error writing to stream" );
+            stream.write_all(&[*byte]);
+            //println!("\nStep 4 passed");
         }
-      }
-
-    let result = stream
-        .write_all(serialized.as_bytes())
-        .expect("erreur envoie message");
-    println!("{result:?} ");
-    match stream.write_all(serialized.as_bytes()) {
-        Ok(()) => println!("Message envoyé avec succès"),
-        Err(err) => eprintln!("Erreur lors de l'envoi final : {}", err),
     }
     reception_message(stream);
 }
@@ -48,42 +45,40 @@ pub fn send_message(mut stream: &TcpStream, message: Message, data: &Vec<u8>) {
 
 pub fn reception_message(mut stream: &TcpStream) {
     // Récupération du message
+    println!("début réception du message\n");
     let mut size_bytes = [0u8; 4];
-    stream.read_exact(&mut size_bytes).expect("Erreur lors de la lecture de la taille du message");
+    stream.read_exact(&mut size_bytes);
     
     let mut new_size_json = [0; 4];
     let mut size_json = u32::from_be_bytes(new_size_json) as usize;
-    stream.read_exact(&mut new_size_json).expect("Erreur lors de la lecture de la taille du message");
+    stream.read_exact(&mut new_size_json);
     
     let msg_size = u32::from_be_bytes(new_size_json) as usize;
-
-    println!("\n msg_size  : {:?}\n", msg_size);
-
     let mut json_buffer = vec![0u8; msg_size as usize];
     stream.read_exact(&mut json_buffer).expect("Erreur lors de la lecture du message JSON");
 
     let img_data: &mut Vec<u8> = &mut Vec::new();  
 
     let data_binaire_taille= msg_size - size_json;
-        if data_binaire_taille> 0 {
+        if data_binaire_taille > 0 {
                 *img_data = vec![0; data_binaire_taille];
                 stream.read_exact(img_data);
         }
-
     // Affichage du message dans la console en tant que chaîne de caractères
     let message = String::from_utf8_lossy(&json_buffer).into_owned();
-    println!("{} ça marche", message);
-
 
     // Traitement du message
     match parse_json_string(&message) {
         Ok(parsed_message) => {
+           
             match parsed_message {
                 Message::FragmentTask(fragment_task) => {
-                    println!("oui ?");
                     // Si le message est de type FragmentTask, calculer le résultat
                     let Ok((fragment_result, data)) = calculate_fragment(&fragment_task) else { todo!() };
                     
+                    println!("\nMessage reçu : \n{:?}", fragment_result);
+                    //println!("data : {:?}", data);
+
                     for i in 0..data.len() {
                         img_data.extend(data[i].zn.to_be_bytes());
                         img_data.extend(data[i].count.to_be_bytes());
